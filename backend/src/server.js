db45eb3731b0ac9a43902a40812907edf3e87813
@douglasabnovato <douglasabnovato@gmail.com>
2019-10-04 11:52:04 -0300

    server js

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-    return res.send('Welcome Omnistack 9.0 !');
+    return res.send('Welcome Omnistack 9.0 !!');
 });
 
 app.listen(3333);
