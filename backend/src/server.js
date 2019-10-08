a36915521af5df79bc20b7c955b6218d009f4eca
@douglasabnovato <douglasabnovato@gmail.com>
2019-10-08 12:30:26 -0300

    criando base da aplicação

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://<username>:<password>@cluster0-oikjh.mongodb.net/test?retryWrites=true&w=majority')
 
 app.use(express.json());
 app.use(routes);
