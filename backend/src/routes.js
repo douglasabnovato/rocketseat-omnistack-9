f3b5b2bc7c5daaa3c94a4601d679331642f44418
@douglasabnovato <douglasabnovato@gmail.com>
2019-10-10 11:10:57 -0300

    criando base da aplicação

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,7 @@
 const express = require('express');
+const SessionController = require('./controllers/SessionController')
 const routes = express.Router();
 
-routes.post('/users', (req,res) => {
-    return res.json(req.body);
-});
+routes.post('/users', SessionController.store);
 
 module.exports = routes;
