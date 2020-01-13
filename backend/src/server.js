f7e64d45a006b82197f3185691c467ac71113b09
@douglasabnovato <douglasabnovato@gmail.com>
2020-01-13 19:25:35 -0300

    backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,9 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const routes = require('./routes');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://omnistack9:omnistack9@cluster0-oikjh.mongodb.net/semana09?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-
-app.use(express.json());
-app.use(routes);
+app.get('/', (req, res) => {
+    
+});
 
 app.listen(3333);
